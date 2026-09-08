a9e9732e62ae37673ea86e497e53ba1ec9e54d59
egortroschin302-hub <egortroschin302@gmail.com>
2026-09-08 16:38:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "planner-v1";
+var CACHE = "planner-combo-v4";
 var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
@@ -6,13 +6,11 @@ self.addEventListener("install", function (e) {
     return self.skipWaiting();
   }));
 });
-
 self.addEventListener("activate", function (e) {
   e.waitUntil(caches.keys().then(function (keys) {
     return Promise.all(keys.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
   }).then(function () { return self.clients.claim(); }));
 });
-
 self.addEventListener("fetch", function (e) {
   if (e.request.method !== "GET") return;
   e.respondWith(
@@ -21,9 +19,7 @@ self.addEventListener("fetch", function (e) {
       caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
       return res;
     }).catch(function () {
-      return caches.match(e.request).then(function (hit) {
-        return hit || caches.match("./index.html");
-      });
+      return caches.match(e.request).then(function (hit) { return hit || caches.match("./index.html"); });
     })
   );
 });
